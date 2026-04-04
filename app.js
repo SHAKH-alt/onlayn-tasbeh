41f2a04e77941af2f22740bc66c59455f6b5559f
SHAKH-alt <shoxruxbek447@gmail.com>
2026-04-04 16:47:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+document.getElementById("btnPlus").addEventListener("click", increaseCount);
+document.getElementById("btnMinus").addEventListener("click", decreaseCount);
+document.getElementById("btnReset").addEventListener("click", resetCount);
+
+let count = 0;
+
+function updateCount() {
+    document.getElementById("count").innerHTML = count;
+}
+
+function increaseCount() {
+    count++;
+    localStorage.setItem("count", count);
+    updateCount();
+}
+
+function decreaseCount() {
+    if ( count > 0 )
+    {count--;}
+    localStorage.setItem("count", count);
+    updateCount();
+}
+
+function resetCount() {
+    count = 0;
+    localStorage.setItem("count", count);
+    updateCount();
+}
+
+window.onload = function () {
+    let saved = localStorage.getItem("count");
+    if (saved !== null) {
+        count = Number(saved);
+    }
+    updateCount();
+};
