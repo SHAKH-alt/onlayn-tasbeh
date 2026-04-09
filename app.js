6e7ab15423079569d69d19006eeabb31523e1b78
SHAKH-alt <shoxruxbek447@gmail.com>
2026-04-09 16:54:10 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+document.getElementById("btnPlus").addEventListener("click", increaseCount);
+document.getElementById("btnMinus").addEventListener("click", decreaseCount);
+document.getElementById("btnReset").addEventListener("click", resetCount);
+
 let count = 0;
 
 function updateCount() {
@@ -30,3 +34,35 @@ window.onload = function () {
     }
     updateCount();
 };
+
+const btn = document.getElementById("btnPlus");
+
+btn.addEventListener("mouseover", () => {
+  btn.style.backgroundColor = "#e74c3c";
+});
+
+btn.addEventListener("mouseout", () => {
+  btn.style.backgroundColor = "#0b62d3e8";
+});
+
+const btn2 = document.getElementById("btnMinus");
+
+btn2.addEventListener("mouseover", () => {
+  btn2.style.backgroundColor = "#0b62d3e8";
+});
+
+btn2.addEventListener("mouseout", () => {
+  btn2.style.backgroundColor = "#e74c3c";
+});
+
+const btn3 = document.getElementById("btnReset");
+
+btn3.addEventListener("mouseover", () => {
+  btn2.style.backgroundColor = "#9f56de";
+  btn.style.backgroundColor = "#9f56de";
+});
+
+btn3.addEventListener("mouseout", () => {
+  btn2.style.backgroundColor = "#e74c3c";
+  btn.style.backgroundColor = "#0b62d3e8";
+});
